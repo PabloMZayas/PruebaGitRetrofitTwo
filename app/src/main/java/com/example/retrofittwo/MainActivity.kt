5530d2cb2de04e9ea9cbec6a0144027bb9117388
PabloZayas <pablo.zayas@shugar.mx>
2022-11-11 14:27:46 -0600

    parte4.2

--- app/src/main/java/com/example/retrofittwo/MainActivity.kt
+++ app/src/main/java/com/example/retrofittwo/MainActivity.kt
@@ -11,8 +11,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://jsonplaceholder.typicode.com/posts/1/"
-private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
